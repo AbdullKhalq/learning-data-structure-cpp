f07d0852323adb07d60358b6a1fb2c2425c65748
AbdullKhalq <mr.abdullkhalq@gmail.com>
2024-11-01 15:28:47 +0300

    Fixed symantic errors and added some comments

--- practice-queue/Main.cpp
+++ practice-queue/Main.cpp
@@ -8,6 +8,7 @@ struct Queue
     int front = -1;
     int rear = -1;
 
+    // Adds an element to the rear of the queue
     void enqueue(int element) {
 
         // when queue is empty
@@ -20,31 +21,37 @@ struct Queue
             cout << "Can't enqueue. Queue overflow" << endl;
         }
         else {
-            queue[++rear] = element;
+            queue[++rear] = element;  // pre-increment
         }
     }
 
+    /* Removes an element from the front of the queue.
+     * That location will have zero after dequeueing even though it cannot be accessed directly.
+     */
     void dequeue() {
         if (front == -1 && rear == -1) {
             cout << "Can't dequeue. Queue underflow" << endl;
         }
         else if (front == rear) {
             cout << "dequeued: " << queue[front] << endl;
-            front = -1;
+            queue[front] = 0;
+            front = -1;  // not sure if it's correct per the college idea
             rear = -1;
         }
         else {
-            cout << "dequeued: " << queue[front--] << endl;
+            cout << "dequeued: " << queue[front] << endl;
+            queue[front++] = 0;  // post-decrement
         }
     }
     
+    // prints from 'front' until 'rear' including rear index
     void displayQueue() {
         if (front == -1 && rear == -1) {
             cout << "Can't display. Queue underflow" << endl;
         }
         else {
             cout << "Queue elements: " << endl;
-            for (int i = 0; i <= rear; i++) {
+            for (int i = front; i <= rear; i++) {
                 cout << queue[i] << endl;
             }
         }
@@ -55,14 +62,23 @@ struct Queue
 int main() {
     Queue q;
 
-    q.displayQueue();
+    // checks if it correctly identify empty queue
+    q.displayQueue();  
 
+    // checks if it works properly & handles overflow
     q.enqueue(5);
-    q.enqueue(5);
+    q.enqueue(6);
+    q.enqueue(7);
+    q.enqueue(8);
+    q.enqueue(9);
+    q.enqueue(10);
+
+    // checks other methods
     q.displayQueue();
     q.dequeue();
     q.displayQueue();
-    q.enqueue(7);
+
+    q.enqueue(7);  // gives overflow because rear is at MAX
     q.displayQueue();
 
 
