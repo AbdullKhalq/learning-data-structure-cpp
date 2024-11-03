66feae9acc2a763f33d49acfc6f0e9d2e22cf308
AbdullKhalq <mr.abdullkhalq@gmail.com>
2024-11-03 10:53:12 +0300

    Added deleteNode for front or rear method for LinkedList

--- practice-list-and-linked-list/List.cpp
+++ practice-list-and-linked-list/List.cpp
@@ -50,15 +50,63 @@ struct LinkedList
         }
     }
 
+    // Delete first item
+    void deleteAtFront() {
+
+        // If the list is empty
+        if (head == NULL) {
+            cout << "Can't deleteAtFront. List is empty" << endl;
+        }
+        // When the list has items. temporarily store to be deleted
+        // node. 
+        // Then, points head to nextNode if exist otherwise NULL
+        else if (head->nextNode == NULL) {
+            Node *tempNode = head;
+            head = NULL;
+            tempNode->nextNode = NULL;
+            delete(tempNode);
+        }
+        else {
+            Node *tempNode = head;
+            head = head->nextNode;
+            tempNode->nextNode = NULL;
+            delete(tempNode);
+        }
+    }
+
+    // Delete last item
+    void deleteAtRear() {
+
+        // If the list is empty
+        if (head == NULL) {
+            cout << "Can't deleteAtFront. List is empty" << endl;
+        }
+        // When the list has items. temporarily store to be deleted
+        // node. 
+        // Then, points head to nextNode if exist otherwise NULL
+        else if (head->nextNode == NULL) {
+            Node *tempNode = head;
+            head = NULL;
+            tempNode->nextNode = NULL;
+            delete(tempNode);
+        }
+        else {
+            Node *tempNode = head;
+            head = head->nextNode;
+            tempNode->nextNode = NULL;
+            delete(tempNode);
+        }
+    }
+
     // Print items from head to tail while the nextNode pointer
     // is not NULL which means it is the last node.
     void displayList() {
-        Node *currentNode = head;
 
-        if (head == NULL && tail == NULL) {
+        if (head == NULL) {
             cout << "Can't displayList. List is empty"  << endl;
         }
         else {
+            Node *currentNode = head;
             cout << "Linked List items: " << endl;
             while (currentNode != NULL) {
                 cout << currentNode->value << ' ';
@@ -73,13 +121,12 @@ struct LinkedList
 int main() {
     LinkedList list;
     list.insertAtRear();
-    list.insertAtRear();
-    
-    list.displayList();
-    
     list.insertAtFront();
 
+    
+    list.displayList();
 
+    list.deleteAtFront();
     list.displayList();
 
     return 0;
